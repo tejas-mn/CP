eacc57ad47bc7b9bfede94fb3c41a5047cf55e7a
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2021-11-29 09:49:15 +0530

    update Cut Ribbon.cpp

--- Dynamic Programming/Cut Ribbon.cpp
+++ Dynamic Programming/Cut Ribbon.cpp
@@ -12,12 +12,12 @@
 #define FAST_IO         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
-const int inf           =1e9 + 10;
-const unsigned int M    =1000000007;
-const int mod           =1e9+7;
-const int INF           =0x3f3f3f3f;
-const ll llINF          =9223372036854775807;
-const int N             =1e5+10;
+const int inf = 1e9 + 10;
+const unsigned int M = 1000000007;
+const int mod = 1e9+7;
+const int INF = 0x3f3f3f3f;
+const ll llINF = 9223372036854775807;
+const int N = 1e5+10;
 
 /*****************************SOLVE HERE*******************************/
 
@@ -52,61 +52,62 @@ int cut(int n , int a , int b , int c)
 
 //DP - topdown
 int t[10000];
-void *adrs = memset(t , -1 , sizeof(t));
+void *adrs = memset(t, -1, sizeof(t));
 
-int cut(int n , int a , int b , int c)
-{
+int cut(int n, int a, int b, int c) {
 
     t[0] = 0;
 
-    for(int i=1;i<=n;i++){
+    for(int i = 1; i <= n; i++) {
 
 
-        int x=INT_MIN,y=INT_MIN,z=INT_MIN;
+        int x = INT_MIN,
+        y = INT_MIN,
+        z = INT_MIN;
 
-        if(i>=a)
-            x = t[i-a];
-        if(i>=b)
-            y = t[i-b];
-        if(i>=c)
-            z = t[i-c];
+        if(i >= a)
+        x = t[i-a];
+        if(i >= b)
+        y = t[i-b];
+        if(i >= c)
+        z = t[i-c];
 
-        t[i] = 1 + max( x , max( y , z ) );
+        t[i] = 1 + max(x, max(y, z));
     }
 
     return t[n];
 }
 
 
-void solve()
-{
+void solve() {
     int n;
     cin>>n;
 
-    int a,b,c;
+    int a,
+    b,
+    c;
     cin>>a>>b>>c;
 
-    cout<<cut(n , a , b , c);
+    cout<<cut(n, a, b, c);
 
-    return ;
+    return;
 }
 
 /*****************************MAIN*************************************/
-int main()
-{
+int main() {
     FAST_IO;
-/****************************STDIN/STDOUT******************************/
+    /****************************STDIN/STDOUT******************************/
     #ifndef ONLINE_JUDGE
     // for getting input from input.txt
     freopen("input.txt", "r", stdin);
     // for writing output to output.txt
     freopen("output.txt", "w", stdout);
     #endif
-/******************************WRITE HERE*********************************/
+    /******************************WRITE HERE*********************************/
     int t;
-    t=1;
+    t = 1;
     //cin>>t;
-    while(t-->0){
+    while(t-->0) {
         solve();
     }
     return 0;
